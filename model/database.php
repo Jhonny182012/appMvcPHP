d6ba2fe4dc4a39e9fc3f5a952880edcab1158d26
Pablo <palandeta@utn.edu.ec>
2018-05-09 12:43:11 -0500

    cambio de cadena de conexion
    cadena de conexion nueva

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=pdomvcalumno;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=mysqlpablo.mysql.database.azure.com;dbname=pdomvcalumno;charset=utf8', 'pablolandeta@mysqlpablo', 'Pablo666');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
